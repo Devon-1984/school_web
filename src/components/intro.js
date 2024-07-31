abc7be1a2387f5f381e253ab037cbd1551d09143
Devon-1984 <deb100.db@gmail.com>
2024-07-31 14:24:43 +0530

    css intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -5,12 +5,12 @@ export default function Intro({ data }) {
   console.log(data);
   return (
     <>
-      <div className="">
+      <div className="mt-32 mb-96 pl-6 pr-6">
+        <Image className="" src={data.heroImgUrl} width={1200} height={1200} />
         <div>
           <div>{data.title}</div>
           <div>{data.description}</div>
         </div>
-        <Image className="card-img" src={data.heroImgUrl} sizes="100vw" />
       </div>
     </>
   );
