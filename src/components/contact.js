ac8c1f258f61360a5dadb406f6b590212438dc63
Devon-1984 <deb100.db@gmail.com>
2024-08-19 14:04:21 +0530

    forms

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Map from "./map";
-import Link from "next/link";
 import Title from "@/components/contactTitle";
 import { useState } from "react";
 import Submitted from "@/components/submitted";
@@ -8,21 +7,24 @@ import Submitted from "@/components/submitted";
 export default function Contact({ data }) {
   const [submitted, setSubmitted] = useState(true);
 
-  function submission() {
-    setSubmitted(false);
-  }
-
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     const formData = new FormData(event.target);
 
-    await fetch("/__forms.html", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    });
+    try {
+      await fetch("/__forms.html", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString(),
+      });
+
+      setSubmitted(false);
+    } catch (error) {
+      console.error("Form submission error:", error);
+    }
   };
+
   return (
     <>
       <div className="md:pl-12 md:pr-12 pl-6 pr-6 mb-5 md:mb-10 mt-[170px] md:flex text-primary-900">
@@ -36,28 +38,28 @@ export default function Contact({ data }) {
               onSubmit={handleSubmit}
               className="max-w-md"
             >
-              <p className="border-b-2 border-primary-900  ">
+              <p className="border-b-2 border-primary-900">
                 <input
                   type="email"
                   name="email"
                   placeholder="Email Address"
-                  className="placeholder-primary-900 text-primary-900 mt-1 w-full focus:outline-none  py-2 md:py-4"
+                  className="placeholder-primary-900 text-primary-900 mt-1 w-full focus:outline-none py-2 md:py-4"
                 />
               </p>
-              <p className="border-b-2 border-primary-900  ">
+              <p className="border-b-2 border-primary-900">
                 <input
                   type="text"
                   name="name"
                   placeholder="Name"
-                  className="placeholder-primary-900 text-primary-900 mt-1 w-full focus:outline-none  py-2 md:py-4"
+                  className="placeholder-primary-900 text-primary-900 mt-1 w-full focus:outline-none py-2 md:py-4"
                 />
               </p>
-              <p className="border-b-2 border-primary-900  ">
+              <p className="border-b-2 border-primary-900">
                 <input
                   type="tel"
                   name="Mobile"
                   placeholder="Mobile"
-                  className="placeholder-primary-900 text-primary-900 mt-1 w-full focus:outline-none  py-2 md:py-4"
+                  className="placeholder-primary-900 text-primary-900 mt-1 w-full focus:outline-none py-2 md:py-4"
                 />
               </p>
               <p>
@@ -72,7 +74,6 @@ export default function Contact({ data }) {
                 <button
                   className="button py-2 px-5 md:px-10 md:py-4 mt-10"
                   type="submit"
-                  onClick={submission}
                 >
                   Submit
                 </button>
