7860ce0087d561b11f213d745908e36180356508
Devon-1984 <deb100.db@gmail.com>
2024-10-04 01:21:03 +0530

    fixed about page

--- src/components/intro.js
+++ src/components/intro.js
@@ -6,18 +6,20 @@ export default function Intro({ data }) {
     <>
       <div className="mt-[170px] md:mb-40 mb-8 pl-6 pr-6 gap-5 text-primary-900 md:flex md:flex-row-reverse md:pl-12 md:pr-12">
         <Image
-          className="md:max-w-sm"
+          className="md:max-w-[30rem] mt-6"
           src={data.aboutImg}
           width={0}
           height={0}
           sizes="100vw"
-          style={{ width: "100%", height: "auto" }}
+          style={{ width: "100%", height: "100%" }}
         />
         <div className="mr-auto mt-auto mb-auto">
           <div className="mt-6 mb-6 font-heading font-medium text-4xl md:text-6xl md:mb-14">
             {data.title}
           </div>
-          <div className="font-body text-lg max-w-xl">{data.description}</div>
+          <div className="font-body text-lg max-w-[50rem]">
+            {data.description}
+          </div>
         </div>
       </div>
     </>
