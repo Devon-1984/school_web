54d1b30823a4fb1a2a0bbec4d6d774c96caa44c7
Devon-1984 <deb100.db@gmail.com>
2024-10-04 01:29:06 +0530

    slight adjustments

--- src/components/intro.js
+++ src/components/intro.js
@@ -4,9 +4,9 @@ import Image from "next/image";
 export default function Intro({ data }) {
   return (
     <>
-      <div className="mt-[170px] md:mb-40 mb-8 pl-6 pr-6 gap-5 text-primary-900 md:flex md:flex-row-reverse md:pl-12 md:pr-12">
+      <div className="mt-[135px] md:mb-40 mb-8 pl-6 pr-6 gap-5 text-primary-900 md:flex md:flex-row-reverse md:pl-12 md:pr-12">
         <Image
-          className="md:max-w-[30rem] mt-6"
+          className="md:max-w-[30rem] mt-20"
           src={data.aboutImg}
           width={0}
           height={0}
