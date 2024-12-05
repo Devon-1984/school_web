ade4bd423c2de13f9503365fa7a5cde9c1c89829
Devon-1984 <deb100.db@gmail.com>
2024-12-05 15:17:40 +0530

    gallery image component improved

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,5 +1,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation, Autoplay } from "swiper/modules";
+import { useNextSanityImage } from "next-sanity-image";
+import { client } from "@/utils/sanity";
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -21,14 +23,7 @@ export const Gallery = ({ images }) => {
       >
         {images.map((image) => (
           <SwiperSlide key={image.asset.url}>
-            <div className="pos-rel br4 ratio-16-9 overflow">
-              <Image
-                src={image.asset.url}
-                fill
-                objectFit="cover"
-                sizes="40vw"
-              />
-            </div>
+            <GalleryImageCard image={image} />
           </SwiperSlide>
         ))}
       </Swiper>
@@ -46,7 +41,7 @@ export const Gallery = ({ images }) => {
               <path
                 d="M13.7812 1.00098L1.99883 12.1856L13.7812 23.3701"
                 stroke="#F5FCFA"
-                stroke-width="2"
+                strokeWidth="2"
               />
             </svg>
           </button>
@@ -61,12 +56,28 @@ export const Gallery = ({ images }) => {
               <path
                 d="M1.34644 23.3701L13.1292 12.1859L1.34644 1.00164"
                 stroke="#F5FCFA"
-                stroke-width="2"
+                strokeWidth="2"
               />
             </svg>
           </button>
         </div>
       </menu>
+    </div>
+  );
+};
+
+const GalleryImageCard = ({ image }) => {
+  const imageProps = useNextSanityImage(client, image);
+
+  return (
+    <div className="pos-rel br4 ratio-16-9 overflow">
+      <Image
+        {...imageProps}
+        className="w-full h-full object-cover"
+        placeholder="blur"
+        blurDataURL={image.asset.metadata.lqip}
+        sizes="40vw"
+      />
     </div>
   );
 };
